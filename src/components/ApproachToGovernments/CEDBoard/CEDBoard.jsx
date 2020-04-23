af6063ffd395ea39bbeb9a642a6b1b93da8e03fe
daniel <dant00300@gmail.com>
2020-04-23 14:47:15 +0900

    Mounted links.

--- src/components/ApproachToGovernments/CEDBoard/CEDBoard.jsx
+++ src/components/ApproachToGovernments/CEDBoard/CEDBoard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import TextBoard from '../../TextBoard';
 import Text from '../../Text';
+import { MUNICIPALITY_CED_LIST, ZERO_CARBON_CITY_LIST } from '../../../constants';
 
 const TitleContainer = styled.div`
   margin-bottom: 2.5em;
@@ -22,8 +23,8 @@ function CEDBoard (props) {
           「宣言」という形をとることで、行政側が気候変動対策への緊急性を認め、
           市民にその危険性を知らせるという意図があります。
           また、議会が採択し宣言することで、その後首長が変わってもその宣言の内容は変わらずその自治体の中で受け継がれます。
-          気候非常事態宣言を発表（議会を通過している）した自治体一覧はこちら
-          2050年二酸化炭素排出実質ゼロを首長が宣言（議会は通過していない）した自治体（ゼロカーボンシティ）一覧はこちら
+          <br />気候非常事態宣言を発表（議会を通過している）した自治体一覧は<u><a href={MUNICIPALITY_CED_LIST} target='_blank'>こちら</a></u>
+          <br />2050年二酸化炭素排出実質ゼロを首長が宣言（議会は通過していない）した自治体（ゼロカーボンシティ）一覧は<u><a href={ZERO_CARBON_CITY_LIST} target='_blank'>こちら</a></u>
         </Text>
       </TextBoard>
     </StyledCEDBoard>
--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 export const DIGITAL_MARCH_URL = 'https://ja.globalclimatestrike.net';
 export const ORGANIZE_GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/1Ru5in0Vbp23ciuVkt1Uv_EJG9LshArgF2MEJraoSAlw/viewform?edit_requested=true';
+export const MUNICIPALITY_CED_LIST = 'https://www.es-inc.jp/ced/index.html';
+export const ZERO_CARBON_CITY_LIST = 'https://www.env.go.jp/policy/zerocarbon.html';
