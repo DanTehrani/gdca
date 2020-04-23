931149b03d47cc046ff0a8feb3ae794326e40ed3
daniel <dant00300@gmail.com>
2020-04-23 14:16:46 +0900

    Updated url.

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const DIGITAL_MARCH_URL = 'https://ja.globalclimatestrike.net/0424digitalstrike/';
+export const DIGITAL_MARCH_URL = 'https://ja.globalclimatestrike.net';
 export const ORGANIZE_GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/1Ru5in0Vbp23ciuVkt1Uv_EJG9LshArgF2MEJraoSAlw/viewform?edit_requested=true';
