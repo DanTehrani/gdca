b93c9b664ad9b157dfb7558ebcf66db90fb0cf3d
daniel <dant00300@gmail.com>
2020-04-29 13:44:33 +0900

    Localized faq page.

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import media from 'styled-media-query';
 import { ORGANIZE_GOOGLE_FORM_URL } from '../constants';
@@ -69,42 +70,6 @@ const TextWrapper = styled.div`
   `};
 `;
 
-const qas = [
-  {
-    question: 'そもそもFridaysForFutureってどういう運動なんですか？',
-    answer: 'FridaysForFutureはスウェーデンの高校生、 \
-    グレタトゥーンベリ（17）が始めた学校ストライキをきっかけに始まった史上最大の気候変動対策運動です。 \
-    2018年8月彼女は「（気候変動のせいで）なくなるかもしれない未来のためにどうして勉強しないといけないのか」と考え、 \
-    学校をストライキしてスウェーデンの国会前で一人で座り込みし、気候危機対策強化・気候正義を訴えました。\
-    その後、彼女の考えや行動に共鳴した世界各地の若者が同様の運動を始め、\
-    昨年の９月では世界760万人が参加する最大の気候運動にまで成長しました。',
-    color: '#0032C8',
-    avatarType: 1
-  },
-  {
-    question: 'FridaysForFuture Japanは何を求めて活動しているの？',
-    answer: '世界の平均気温上昇を産業革命前に比べて1.5℃に抑え、\
-    気候危機の被害を抑えるために、日本の気候政策を変えることです。​​',
-    color: '#C80032',
-    avatarType: 2
-  },
-  {
-    question: 'FridaysForFuture Japanの活動にはどうやって参加すれば良いですか？',
-    answer: (<span>オーガナイザーとして活動したいと考えている方は、
-    ぜひ<u><a href={ORGANIZE_GOOGLE_FORM_URL} target='_blank'>こちら</a></u>のフォームからご応募ください。
-    さらに詳しくは、<Link to='to-organize-form'>こちら</Link>をご確認ください。</span>),
-    color: '#007850',
-    avatarType: 3
-  },
-  {
-    question: 'FridaysForFuture Japanに代表はいるんですか？',
-    answer: '活動のためにどれだけの時間をかけられるかには個人差がありますが、\
-    私たちの間には上下関係はなく、代表もいません。​',
-    avatarType: 4,
-    color: '#C80032'
-  }
-]
-
 function FAQ () {
   const isLargeScreen = useMediaQuery({ query: '(max-width: 1170px)' });
   const isTablet = useMediaQuery({ query: '(max-width: 768px)' });
@@ -113,11 +78,46 @@ function FAQ () {
   const containerWidth =  isTablet ? '700px' :
     isLargeScreen ? '980px' : '1240px';
 
+  const { t } = useTranslation();
+
+  const qas = [
+    {
+      question: t('FAQ.Q1'),
+      answer: t('FAQ.A1'),
+      color: '#0032C8',
+      avatarType: 1
+    },
+    {
+      question: t('FAQ.Q2'),
+      answer: t('FAQ.A2'),
+      color: '#C80032',
+      avatarType: 2
+    },
+    {
+      question: t('FAQ.Q3'),
+      answer: <span>
+        {t('FAQ.A3_1')}
+        <a href={ORGANIZE_GOOGLE_FORM_URL} target='_blank'><u>{t('FAQ.link1')}</u></a>
+        {t('FAQ.A3_2')}
+        <a href='to-organize-form'><u>{t('FAQ.link2')}</u></a>
+        {t('FAQ.A3_3')}
+        </span>,
+      color: '#007850',
+      avatarType: 3
+    },
+    {
+      question: t('FAQ.Q4'),
+      answer: t('FAQ.A4'),
+      avatarType: 4,
+      color: '#C80032'
+    }
+  ]
+
   return (
     <Page maxWidth='1120px'>
       <HeaderContainer><Header /></HeaderContainer>
         <StyledFAQ>
-          <TextContainer><TextWrapper><Text>みなさんからよくいただく<br />質問にお答えします！</Text></TextWrapper></TextContainer>
+          <TextContainer><TextWrapper><Text>{t('FAQ.ANSWER FAQ Row1')}<br />{t('FAQ.ANSWER FAQ Row2')}</Text></TextWrapper></TextContainer>
           {!isMobile && <FloatingFAQImage containerWidth={containerWidth} top='7em'/>}
           {
             qas.map((qa, i) =>
