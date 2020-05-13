7d5a39de1ba3822a14837cba4a226de9d5599036
daniel <dant00300@gmail.com>
2020-05-13 10:47:48 +0900

    Fade in mobile share bar on scroll.

--- src/components/MobileShareBar/MobileShareBar.jsx
+++ src/components/MobileShareBar/MobileShareBar.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components';
-import { useTranslation } from 'react-i18next';
+import { withTranslation } from 'react-i18next';
 import { withRouter } from 'react-router-dom';
 import TwitterShareButton from '../SocialIconButtons/TwitterShareButton';
 import FacebookShareButton from '../SocialIconButtons/FacebookShareButton';
 import { Share } from '@styled-icons/boxicons-solid/Share';
+import Fade from 'react-reveal/Fade';
 import Text from '../Text';
 import { FACEBOOK_SHARE_URL } from '../../constants';
 import { constructTwitterShareURL } from '../utils';
 
 const StyledMobileShareBar = styled.div`
+  display: ${props => props.hidden ? 'none' : 'flex'};
   position: fixed;
   bottom: 1em;
   right: 0;
-  display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
@@ -32,26 +33,53 @@ const StyledText = styled(Text)`
   font-size: 1em;
 `;
 
-function MobileShareBar (props) {
-  const { t } = useTranslation();
-  const { pathname } = props.location;
-  const twitterShareURL = constructTwitterShareURL(t(`Sidebar.ShareText.${pathname}`), window.location.href);
+class MobileShareBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollTop: 0
+    };
+  }
 
-  return (
-    <StyledMobileShareBar>
-      <StyledText>{t('Sidebar.share')}</StyledText>
-      <TwitterButtonContainer>
-        <TwitterShareButton
-          width='3.2em'
-          onClick={() => {window.open(twitterShareURL)}} />
-      </TwitterButtonContainer>
-      <FacebookButtonContainer>
-        <FacebookShareButton
-          width='3.2em'
-          onClick={() => {window.open(FACEBOOK_SHARE_URL)}}/>
-      </FacebookButtonContainer>
-    </StyledMobileShareBar>
-  )
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    const doc = document.documentElement;
+    const scrollTop = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+
+    this.setState({ scrollTop });
+  }
+
+  render () {
+    const { t } = this.props;
+    const { pathname } = this.props.location;
+    const twitterShareURL = constructTwitterShareURL(t(`Sidebar.ShareText.${pathname}`), window.location.href);
+
+    const show = this.state.scrollTop > 50;
+    return (
+        <StyledMobileShareBar hidden={!show}>
+          <Fade bottom>
+            <StyledText>{t('Sidebar.share')}</StyledText>
+            <TwitterButtonContainer>
+              <TwitterShareButton
+                width='3.2em'
+                onClick={() => {window.open(twitterShareURL)}} />
+            </TwitterButtonContainer>
+            <FacebookButtonContainer>
+              <FacebookShareButton
+                width='3.2em'
+                onClick={() => {window.open(FACEBOOK_SHARE_URL)}}/>
+            </FacebookButtonContainer>
+          </Fade>
+        </StyledMobileShareBar>
+      )
+  }
 }
 
-export default withRouter(MobileShareBar);
+export default withRouter(withTranslation()(MobileShareBar));
