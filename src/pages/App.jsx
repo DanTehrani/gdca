247f3f34af4dd78175f30c5e0880d56d208ba4c2
Daniel Tehrani <contact@dantehrani.com>
2020-08-13 15:04:48 +0900

    Lazy loading and font swap.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+/* eslint-disable import/first */
+
+import React, { Suspense, lazy } from 'react';
+const Top = lazy(() => import('./Top'));
+const AboutUs = lazy(() => import('./AboutUs'));
+const FAQ = lazy(() => import('./FAQ'));
+const ToOrganizeForm = lazy(() => import('./ToOrganizeForm'));
+const PrivacyPolicy = lazy(() => import('./PrivacyPolicy'));
+const ApproachToGovernments = lazy(() => import('./ApproachToGovernments'));
+const Actions = lazy(() => import('./Actions'));
+const PressRelease200623 = lazy(() => import('./Articles/PressRelease200623'));
+const PressRelease200626 = lazy(() => import('./Articles/PressRelease200626'));
+const Opinion200626 = lazy(() => import('./Articles/Opinion200626'));
+const GDAApplicationForm = lazy(() => import('./GDAApplicationForm'));
 import { ThemeProvider } from 'styled-components'
 import './App.css'
 import '../i18n'
@@ -8,17 +21,6 @@ import {
   Route
 } from 'react-router-dom'
 import ScrollTop from '../components/ScrollTop'
-import Top from './Top'
-import AboutUs from './AboutUs'
-import FAQ from './FAQ'
-import ToOrganizeForm from './ToOrganizeForm'
-import PrivacyPolicy from './PrivacyPolicy'
-import ApproachToGovernments from './ApproachToGovernments'
-import Actions from './Actions'
-import PressRelease200623 from './Articles/PressRelease200623'
-import PressRelease200626 from './Articles/PressRelease200626'
-import Opinion200626 from './Articles/Opinion200626'
-import GDAApplicationForm from './GDAApplicationForm'
 
 const theme = {
   textColor: '#007850',
@@ -39,43 +41,45 @@ function App () {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <ScrollTop>
-          <Switch>
-            <Route exact path='/'>
-              <Top />
-            </Route>
-            <Route path='/about-us'>
-              <AboutUs />
-            </Route>
-            <Route path='/faq'>
-              <FAQ />
-            </Route>
-            <Route path='/to-organize-form'>
-              <ToOrganizeForm />
-            </Route>
-            <Route path='/privacy-policy'>
-              <PrivacyPolicy />
-            </Route>
-            <Route path='/approach-to-governments'>
-              <ApproachToGovernments />
-            </Route>
-            <Route path='/actions'>
-              <Actions />
-            </Route>
-            <Route path='/press-release-200623'>
-              <PressRelease200623 />
-            </Route>
-            <Route path='/press-release-200626'>
-              <PressRelease200626 />
-            </Route>
-            <Route path='/opinion-200626'>
-              <Opinion200626 />
-            </Route>
-            <Route path='/gda-application-form'>
-              <GDAApplicationForm />
-            </Route>
-          </Switch>
-        </ScrollTop>
+        <Suspense fallback={<></>}>
+          <ScrollTop>
+            <Switch>
+              <Route exact path='/'>
+                <Top />
+              </Route>
+              <Route path='/about-us'>
+                <AboutUs />
+              </Route>
+              <Route path='/faq'>
+                <FAQ />
+              </Route>
+              <Route path='/to-organize-form'>
+                <ToOrganizeForm />
+              </Route>
+              <Route path='/privacy-policy'>
+                <PrivacyPolicy />
+              </Route>
+              <Route path='/approach-to-governments'>
+                <ApproachToGovernments />
+              </Route>
+              <Route path='/actions'>
+                <Actions />
+              </Route>
+              <Route path='/press-release-200623'>
+                <PressRelease200623 />
+              </Route>
+              <Route path='/press-release-200626'>
+                <PressRelease200626 />
+              </Route>
+              <Route path='/opinion-200626'>
+                <Opinion200626 />
+              </Route>
+              <Route path='/gda-application-form'>
+                <GDAApplicationForm />
+              </Route>
+            </Switch>
+          </ScrollTop>
+        </Suspense>
       </BrowserRouter>
     </ThemeProvider>
   )
