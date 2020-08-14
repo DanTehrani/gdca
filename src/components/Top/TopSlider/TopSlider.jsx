da9d4c24016c31d792bf6c440a99ec4f8ea8a727
Daniel Tehrani <contact@dantehrani.com>
2020-08-14 10:14:35 +0900

    Removed yellow GDA slider.

--- src/components/Top/TopSlider/TopSlider.jsx
+++ src/components/Top/TopSlider/TopSlider.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import GDABanner from '../GDABanner'
 import GDABannerB from '../GDABannerB'
 import LargeImage from '../LargeImage'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
@@ -9,7 +8,6 @@ import { Carousel } from 'react-responsive-carousel';
 function TopSlider () {
   return (
     <Carousel showArrows={true} infiniteLoop={true} autoPlay={true} interval={3000} showStatus={false}>
-      <GDABanner/>
       <GDABannerB />
       <LargeImage />
     </Carousel>
