6d69bf0d6c928b576eced107615c932c67561169
Daniel Tehrani <contact@dantehrani.com>
2020-08-20 22:29:18 +0900

    Using blue earth banner.

--- src/components/Top/TopSlider/TopSlider.jsx
+++ src/components/Top/TopSlider/TopSlider.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import GDABannerB from '../GDABannerB'
 import LargeImage from '../LargeImage'
-import EndorseBanner from '../EndorseBanner'
 import EndorseBannerB from '../EndorseBannerB'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
@@ -10,7 +9,6 @@ import { Carousel } from 'react-responsive-carousel';
 function TopSlider () {
   return (
     <Carousel showArrows={true} infiniteLoop={true} autoPlay={true} interval={3000} showStatus={false}>
-      <EndorseBanner />
       <EndorseBannerB />
       <GDABannerB />
       <LargeImage />
