7f3e344baaa647f75586bde32bd54f1dbff5ea0f
Daniel Tehrani <contact@dantehrani.com>
2020-08-20 20:31:56 +0900

    Added banner.

--- src/components/Top/TopSlider/TopSlider.jsx
+++ src/components/Top/TopSlider/TopSlider.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import styled from 'styled-components'
 import GDABannerB from '../GDABannerB'
 import LargeImage from '../LargeImage'
+import EndorseBanner from '../EndorseBanner'
+import EndorseBannerB from '../EndorseBannerB'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
 function TopSlider () {
   return (
     <Carousel showArrows={true} infiniteLoop={true} autoPlay={true} interval={3000} showStatus={false}>
+      <EndorseBanner />
+      <EndorseBannerB />
       <GDABannerB />
       <LargeImage />
     </Carousel>
