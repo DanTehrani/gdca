b151a32182c4f0ecff8d98fb00f484ee99e0183a
Daniel Tehrani <contact@dantehrani.com>
2020-08-21 16:21:32 +0900

    Updated arrows.

--- src/components/Top/TopSlider/TopSlider.jsx
+++ src/components/Top/TopSlider/TopSlider.jsx
@@ -5,10 +5,32 @@ import LargeImage from '../LargeImage'
 import EndorseBannerB from '../EndorseBannerB'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import { RightArrow } from '@styled-icons/boxicons-regular/RightArrow'
+import './style.css'
+
+const PrevArrowTemplate = styled(RightArrow)
+``
+
+const NextArrowTemplate = styled.button
+``
+
+const renderArrowPrev = () => {
+  return <PrevArrowTemplate />
+}
+
+const renderArrowNext = () => {
+  return <NextArrowTemplate/>
+}
 
 function TopSlider () {
   return (
-    <Carousel showArrows={true} infiniteLoop={true} autoPlay={true} interval={3000} showStatus={false}>
+    <Carousel 
+      showArrows={true} 
+      infiniteLoop={true} 
+      autoPlay={true} 
+      interval={3000} 
+      showStatus={false}
+      >
       <EndorseBannerB />
       <GDABannerB />
       <LargeImage />
