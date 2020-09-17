1a6ea97ebf56eb70494880be71c4ce70e142a837
Daniel Tehrani <contact@dantehrani.com>
2020-09-17 12:02:32 +0900

    Patagonia actions.

--- src/components/GDAApplicationForm/ActionsList/ActionsList.jsx
+++ src/components/GDAApplicationForm/ActionsList/ActionsList.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import media from 'styled-media-query'
 import { useMediaQuery } from 'react-responsive'
+import Text from '../../Text'
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -40,6 +41,15 @@ const StyledTableCellOrg = styled(TableCell)
 `
   min-width: 100px;
 `
+
+const StyledText = styled(Text)
+`
+  display: block;
+  font-size: 0.8em;
+  color: ${props => props.theme.black};
+  margin: 1em 0;
+`
+
 
 function ActionsList () {
   const rows = [
@@ -190,6 +200,121 @@ function ActionsList () {
     }
   ]
   
+  const actionListPatagonia = [
+    {
+      time: '',
+      actionTitle: 'パタゴニア 札幌北　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 札幌北',
+      orgURL: 'https://www.patagonia.jp/patagonia-sapporo-kita-japan/store_924604544.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 仙台　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 仙台',
+      orgURL: 'https://www.patagonia.jp/patagonia-sendai-japan/store_943024081.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア サーフ千葉／アウトレット　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア サーフ千葉／アウトレット',
+      orgURL: 'https://www.patagonia.jp/patagonia-surf-chiba-outlet-japan/store_164343035.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 東京・渋谷　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 東京・渋谷',
+      orgURL: 'https://www.patagonia.jp/patagonia-shibuya-japan/store_924604555.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア サーフ東京／アウトレット　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア サーフ東京／アウトレット',
+      orgURL: 'https://www.patagonia.jp/patagonia-surf-tokyo-outlet-japan/store_924604566.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 東京・ゲートシティ大崎　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 東京・ゲートシティ大崎',
+      orgURL: 'https://www.patagonia.jp/patagonia-tokyo-gate-city-ohsaki-japan/store_1049259465.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 東京・二子玉川　　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 東京・二子玉川',
+      orgURL: 'https://www.patagonia.jp/patagonia-futakotamagawa-japan/store_399347574.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 鎌倉　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 鎌倉',
+      orgURL: 'https://www.patagonia.jp/patagonia-kamakura-japan/store_924604452.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 横浜・関内　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 横浜・関内',
+      orgURL: 'https://www.patagonia.jp/patagonia-yokohama-kannai-japan/store_924604499.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 名古屋　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 名古屋',
+      orgURL: 'https://www.patagonia.jp/patagonia-nagoya-japan/store_924604232.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 京都　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 京都',
+      orgURL: 'https://www.patagonia.jp/patagonia-kyoto-japan/store_164344596.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 大阪　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 大阪',
+      orgURL: 'https://www.patagonia.jp/patagonia-osaka-japan/store_924604522.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア サーフ大阪／アウトレット　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア サーフ大阪／アウトレット',
+      orgURL: 'https://www.patagonia.jp/patagonia-surf-osaka-outlet-japan/store_399347575.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 神戸　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 神戸',
+      orgURL: 'https://www.patagonia.jp/patagonia-kobe-japan/store_924604466.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 広島　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 広島',
+      orgURL: 'https://www.patagonia.jp/patagonia-hiroshima-japan/store_1845547797.html'
+    },
+    {
+      time: '',
+      actionTitle: 'パタゴニア 福岡　シューズアクション',
+      eventPageURL: '',
+      org: 'パタゴニア 福岡',
+      orgURL: 'https://www.patagonia.jp/patagonia-fukuoka-japan/store_924604588.html'
+    }
+  ]
+  
   return (
     <Container>
     <StyledTable>
@@ -219,7 +344,32 @@ function ActionsList () {
           ))
         }
       </TableBody>
-    </StyledTable>
+      </StyledTable>
+      <StyledText bold>
+        パタゴニア店舗で行われるアクションでは、コロナ禍における店舗の運営を鑑みて、
+      申し訳ありませんがシューズの持ち込みをご遠慮いただいています。
+      </StyledText>
+      <StyledTable>
+        <TableBody>
+          {
+            actionListPatagonia.map((row, i) => (
+              <TableRow key={i}>
+                <StyledTableCellTime>{row.time}</StyledTableCellTime>
+                {
+                  row.eventPageURL?
+                  <StyledTableCellActionTitle><a href={row.eventPageURL} target="_blank">{row.actionTitle}</a></StyledTableCellActionTitle>
+                  :<StyledTableCellActionTitle>{row.actionTitle}</StyledTableCellActionTitle>
+                }
+                {
+                  row.orgURL?
+                  <StyledTableCellOrg><a href={row.orgURL} target="_blank">{row.org}</a></StyledTableCellOrg>
+                  :<StyledTableCellOrg>{row.org}</StyledTableCellOrg>
+                }
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </StyledTable>
     </Container>
     )
 }
