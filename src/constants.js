9e30487aebf2581bc24f5495e7c7139047f24c06
Daniel Tehrani <contact@dantehrani.com>
2020-11-14 20:51:07 +0900

    Updated top video.

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const ZERO_CARBON_CITY_LIST = 'https://www.env.go.jp/policy/zerocarbon.ht
 export const DIGITAL_STRIKE_YOUTUBE = 'https://www.youtube.com/watch?v=xpYNJepgPmM&feature=emb_title'
 export const FACEBOOK_SHARE_URL = 'https://www.facebook.com/share.php?u=https://fridaysforfuture.jp/'
 export const YOUTUBE_CHANNEL_LINK = 'https://www.youtube.com/channel/UCmAxzODI33fBPXrXefFKgog'
-export const VIDEO_A_URL = 'https://www.youtube.com/watch?v=TLdvpdU-rH0'
+export const VIDEO_A_URL = 'https://www.youtube.com/watch?v=peVuQzcNO2I&feature=emb_title'
 export const VIDEO_B_URL = 'https://www.youtube.com/watch?v=YUkruYxI5B4'
 export const MEGA_BANKS_SIGNATURE_URL = 'https://act.350.org/sign/jp-bank-agms2020/'
 export const NOTE_ACCOUNT_LINK = 'https://note.com/fffjapan'
